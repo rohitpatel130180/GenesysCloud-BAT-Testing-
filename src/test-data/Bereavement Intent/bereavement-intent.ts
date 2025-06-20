730d4397e4032eecde6291db56231618879b8bfa
Rohit Patel <rohit.patel@ovo.com>
2025-06-20 14:46:32 +0100

    Added code for Berevemet 20/06/2025

---
+++ src/test-data/Bereavement Intent/bereavement-intent.ts
@@ -0,0 +1,11 @@
+export const bereavementIntent_138541_Data = [
+    {
+        "Msg-If You Need To Let Us Know": "If you need to let us know that one of our customers has passed away, we’re here to help you through the next steps.",
+        "Que-Have You Already Contacted?": "Have you already contacted us to advise someone has passed away?",
+        "Msg-LifeLedger Is A Free Online Tool": "LifeLedger is a free online tool that lets you get in touch with us and other companies, to tell us that someone has passed away.\nIt means you can notify multiple companies from one place:\nhttps://lifeledger.com/bereavement/energy/registering-a-death-with-ovo-energy/.",
+        "Que-Did You get The Help You Needed": "Did you get the help you needed?",
+        "Que-Is There Anything Else": "Is there anything else I can help with?",
+        "Msg-Thank You For Contacting": "Thank you for contacting OVO Energy. If you have any further questions just ask me anytime...",
+        "Que-Do You Still Need Help":"Do you still need help and would like to talk to one of our agents?",
+    },
+]
